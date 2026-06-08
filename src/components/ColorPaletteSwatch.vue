3e423a671619ecf71b6884351426a7a34b78b01a
michel <michel875562520520@gmail.com>
2026-06-08 22:27:21 +0800

    feat(ui): 新增 ColorPaletteSwatch 元件

---
+++ src/components/ColorPaletteSwatch.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  colors: string[]
+}>()
+</script>
+
+<template>
+  <div class="p-4 bg-elevated rounded">
+    <p class="text-caption font-mono text-text-secondary mb-2">color palette</p>
+    <div v-if="colors.length" class="flex h-16 rounded overflow-hidden">
+      <div
+        v-for="(color, i) in colors"
+        :key="i"
+        data-testid="color-block"
+        class="flex-1"
+        :style="{ backgroundColor: color }"
+      />
+    </div>
+  </div>
+</template>
