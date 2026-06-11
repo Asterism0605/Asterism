cc003890a81bc1fac66547e15b25b59056066266
Una <ruwen.hsieh@gmail.com>
2026-06-11 21:34:42 +0800

    feat(dna): 新增測驗型別

---
+++ src/types/style-dna.ts
@@ -0,0 +1,16 @@
+import type {
+  DNAAnswer,
+  DNAImage,
+  DNAQuestion,
+  DNAResult,
+  DNAStyleScore,
+  DNATestOption,
+} from '@/types/dna-test'
+
+export type StyleDnaImage = DNAImage
+export type StyleDnaOption = DNATestOption
+export type StyleDnaQuestion = DNAQuestion
+export type StyleDnaAnswer = DNAAnswer
+export type StyleDnaStyleScore = DNAStyleScore
+export type StyleDnaResult = DNAResult
+
