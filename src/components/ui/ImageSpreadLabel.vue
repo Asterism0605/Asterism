d02814bb7e7cd8a1a12cd044c1549b861f3cf768
Una <ruwen.hsieh@gmail.com>
2026-07-20 12:55:25 +0800

    feat(style): 調整手機版 label 跑版問題

--- src/components/ui/ImageSpreadLabel.vue
+++ src/components/ui/ImageSpreadLabel.vue
@@ -11,11 +11,11 @@ withDefaults(
 <template>
   <component
     :is="as"
-    class="absolute bottom-[5px] left-[5px] z-10 inline-grid h-[26px] max-w-[calc(100%-24px)] items-end justify-items-start rounded-[1px_9px_1px_1px] bg-black/90 pb-[3px] pl-[7px] pr-9 text-text-primary"
+    class="absolute bottom-[5px] left-[5px] z-10 inline-grid max-w-[calc(100%-24px)] items-end justify-items-start rounded-[1px_9px_1px_1px] bg-black/90 pt-[9px] pb-[3px] pl-[7px] pr-9 text-text-primary"
     :class="{ 'origin-bottom-left scale-[1.3]': size === 'large' }"
   >
     <span
-      class="min-w-0 max-w-full font-title text-[11px] font-normal lowercase tracking-normal [[lang=zh-Hant]_&]:text-[10px]"
+      class="min-w-0 max-w-full font-title text-[11px] font-normal leading-[1.2] lowercase tracking-normal lg:leading-normal [[lang=zh-Hant]_&]:text-[10px]"
     >
       <slot />
     </span>
